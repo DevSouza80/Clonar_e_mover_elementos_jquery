37ea05dc3b575aefec9109724afc65bcc0f5ead9
Pedro Henrique Dias de Souza <desenvolvedorweb7070@hotmail.com>
2026-04-15 21:45:37 -0300

    Update func.js

--- js/func.js
+++ js/func.js
@@ -1,33 +1,9 @@
-// Função principal usada nos eventos
-var func = function(){
-    // Muda a cor de fundo do input
-    $(this).css('background','green');
+$(function(){
 
-    // Mostra o índice do input
-    console.log($(this).index());
-};
+  // Clona o elemento
+  var el = $('#source .texto').clone();
 
-// Evento keyup
-$('input[name=nome_pessoa]').keyup(func);
+  // Adiciona a cópia no container
+  el.appendTo('#container');
 
-// Evento keyup para todos inputs de texto
-$('input[type=text]').keyup(func);
-
-// Evento keydown
-$('input[type=text]').keydown(func);
-
-// Evento de clique direto
-$('a').click(function(){
-    alert('Olá mundo!');
 });
-
-// Delegação de evento (funciona com elementos criados depois)
-$('body').on('click','a',function(){
-    alert('Olá mundo');
-    return false;
-});
-
-// Criando elemento dinamicamente após 3 segundos
-setTimeout(function(){
-    $('body').html('<a href="#">Meu link!</a>');
-},3000);
